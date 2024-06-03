3f921593471b790f2004c0c590a68e4b6d2b6179
DavosJar <alexis.jara@unl.edu.ec>
2024-06-02 22:52:13 -0500

    Fin

--- SistemaHotel/client_manager/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from client_manager.models import *
-# Register your models here.
-admin.site.register(Cliente)
-admin.site.register(Historial)
-
--- SistemaHotel/client_manager/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClientManagerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'client_manager'
